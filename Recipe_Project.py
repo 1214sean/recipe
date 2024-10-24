dc800e9b98710daaddaa14e3c4a79ee90ff0df7f
aboleng <153039681+aboleng@users.noreply.github.com>
2024-10-24 12:52:06 -0400

    Update Recipe_Project.py

--- Recipe_Project.py
+++ Recipe_Project.py
@@ -1,21 +1,21 @@
 class Recipe:
     def __init__(rec,name,notes):
-        rec.name=name
-        rec.notes=notes
+        rec._name=name
+        rec._notes=notes
     
 class Ingredients(Recipe):
     def __init_subclass__(ing,quant,unit,type):
-        ing.quant=quant
-        ing.unit=unit
-        ing.type=type
+        ing._quant=quant
+        ing._unit=unit
+        ing._type=type
 
 class ShoppingList(Recipe):
     def __init_subclass__(slist,amount,measure,item):
-        slist.amount=amount
-        slist.measure=measure
-        slist.item=item
+        slist._amount=amount
+        slist._measure=measure
+        slist._item=item
 
 class Procedure(Recipe):
     def __init_subclass__(plist,step_num,step):
-        plist.step_num=step_num
-        plist.step=step
+        plist._step_num=step_num
+        plist._step=step
